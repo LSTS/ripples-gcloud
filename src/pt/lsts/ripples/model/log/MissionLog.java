3390f62adb7ed62f90913620c3c78fb91c7152e1
José Pinto <zepinto@gmail.com>
2015-03-18 16:24:51 +0000

    Added CRUD operations on logbooks.

--- src/pt/lsts/ripples/model/log/MissionLog.java
+++ src/pt/lsts/ripples/model/log/MissionLog.java
@@ -9,6 +9,8 @@ import java.util.TimeZone;
 
 import com.github.rjeschke.txtmark.Processor;
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.googlecode.objectify.annotation.Cache;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
@@ -16,11 +18,12 @@ import com.googlecode.objectify.annotation.Id;
 @Entity
 @Cache
 public class MissionLog {
-	private static final SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd");
-	
+	private static final SimpleDateFormat sdf = new SimpleDateFormat(
+			"YYYY-MM-dd");
+
 	@Id
 	public String date = sdf.format(new Date());
-	
+
 	public String place = "";
 	public String conditions = "";
 	public ArrayList<String> objectives = new ArrayList<>();
@@ -28,89 +31,94 @@ public class MissionLog {
 	public ArrayList<String> systems = new ArrayList<>();
 	public ArrayList<LogEntry> log = new ArrayList<>();
 	public ArrayList<ActionItem> actions = new ArrayList<>();
-	
-	
+
 	public String asMarkDown() {
 		SimpleDateFormat hourFormat = new SimpleDateFormat("HH:mm");
 		hourFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-		
+
 		StringBuilder buf = new StringBuilder();
 		buf.append("Mission Report\n");
 		buf.append("================\n\n");
-		
+
 		buf.append("Date\n");
 		buf.append("----\n\n");
-		buf.append(date.trim()+"\n\n");
-		
+		buf.append(date.trim() + "\n\n");
+
 		buf.append("Place\n");
 		buf.append("-----\n\n");
-		buf.append(place.trim()+"\n\n");
-		
+		buf.append(place.trim() + "\n\n");
+
 		buf.append("Team\n");
 		buf.append("----\n\n");
 		for (String teamMember : team)
-			buf.append("* " + teamMember.trim()+"\n");
-		
+			buf.append("* " + teamMember.trim() + "\n");
+
 		buf.append("Systems\n");
 		buf.append("-------\n\n");
 		for (String system : systems)
-			buf.append("* " + system.trim()+"\n");
+			buf.append("* " + system.trim() + "\n");
 		buf.append("\n");
-		
+
 		buf.append("Conditions\n");
 		buf.append("----------\n\n");
-		buf.append(conditions.trim()+"\n");
-		
+		buf.append(conditions.trim() + "\n");
+
 		buf.append("\n");
 		buf.append("Timezone\n");
 		buf.append("--------\n\nUTC\n\n");
-		
+
 		buf.append("Objectives\n");
 		buf.append("----------\n\n");
 		for (String objective : objectives)
-			buf.append("* " + objective.trim()+"\n");
+			buf.append("* " + objective.trim() + "\n");
 		buf.append("\n");
-		
+
 		buf.append("Log Book\n");
 		buf.append("--------\n\n");
-		
+
 		for (LogEntry entry : log) {
-			String date = "*"+hourFormat.format(entry.timestamp)+"*";
+			String date = "*" + hourFormat.format(entry.timestamp) + "*";
 			if (entry.text.trim().contains("\n"))
-				buf.append("* "+date+" \n>"+entry.text.trim()+"\n");
+				buf.append("* " + date + " \n>" + entry.text.trim() + "\n");
 			else
-				buf.append("* "+date+" "+entry.text.trim()+"\n");
+				buf.append("* " + date + " " + entry.text.trim() + "\n");
 		}
 		buf.append("\n");
 		buf.append("Action Items\n");
 		buf.append("------------\n");
-		
+
 		LinkedHashMap<String, ArrayList<ActionItem>> itemsPerModule = new LinkedHashMap<>();
 		for (ActionItem item : actions) {
-			String moduleNormalized = item.module.trim().toLowerCase(); 
+			String moduleNormalized = item.module.trim().toLowerCase();
 			if (!itemsPerModule.containsKey(moduleNormalized))
-				itemsPerModule.put(moduleNormalized, new ArrayList<ActionItem>());
+				itemsPerModule.put(moduleNormalized,
+						new ArrayList<ActionItem>());
 			itemsPerModule.get(moduleNormalized).add(item);
 		}
-		
-		for (Entry<String, ArrayList<ActionItem>> action : itemsPerModule.entrySet()) {
-			buf.append("* "+action.getValue().get(0).module+":\n");
+
+		for (Entry<String, ArrayList<ActionItem>> action : itemsPerModule
+				.entrySet()) {
+			buf.append("* " + action.getValue().get(0).module + ":\n");
 			for (ActionItem item : action.getValue()) {
-				buf.append("   * "+item.text.trim()+"\n");
+				buf.append("   * " + item.text.trim() + "\n");
 			}
 		}
-		
+
 		return buf.toString();
 	}
-	
+
 	public String asHtml() {
 		return Processor.process(asMarkDown());
 	}
-	
+
+	public JsonObject asJsonObject() {
+		return new Gson().toJsonTree(this).getAsJsonObject();
+	}
+
 	public String asJson() {
 		return new Gson().toJson(this);
 	}
-	
+
 	public static void main(String[] args) {
 		MissionLog log = new MissionLog();
 		log.conditions = "clear sky.";
@@ -129,11 +137,11 @@ public class MissionLog {
 		entry.author = "ZP";
 		entry.text = "oops... sometyhing wrong happened.";
 		log.log.add(entry);
-		
+
 		System.out.println(log.asHtml());
-		
+
 		System.out.println(log.asMarkDown());
-		
+
 		System.out.println(log.asJson());
-	}	
+	}
 }
--- src/pt/lsts/ripples/servlets/LogbookServlet.java
+++ src/pt/lsts/ripples/servlets/LogbookServlet.java
@@ -1,137 +1,280 @@
 package pt.lsts.ripples.servlets;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import pt.lsts.ripples.model.HubSystem;
-import pt.lsts.ripples.model.JsonUtils;
 import pt.lsts.ripples.model.Store;
+import pt.lsts.ripples.model.log.ActionItem;
+import pt.lsts.ripples.model.log.LogEntry;
 import pt.lsts.ripples.model.log.MissionLog;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 public class LogbookServlet extends HttpServlet {
 	private static final long serialVersionUID = 307618046379130819L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		if (!req.getContentType().equals("application/json")) {
+			resp.sendError(400, "Invalid content type: " + req.getContentType()
+					+ ". Only JSON is allowed.");
+			return;
+		}
+
+		MissionLog log = getLogBook(req);
+
+		if (log == null) {
+			resp.sendError(400, "Logbook does not exist.");
+			return;
+		}
+
+		if (req.getPathInfo().split("/").length > 2) {
+			String member = req.getPathInfo().split("/")[2].toLowerCase();
+			int indexToDelete = -1;
+			if (req.getPathInfo().split("/").length > 4
+					&& req.getPathInfo().split("/")[3].equals("delete")) {
+				try {
+					indexToDelete = Integer.parseInt(req.getPathInfo().split(
+							"/")[4]);
+				} catch (Exception e) {
+					resp.sendError(500, e.getMessage());
+				}
+			}
+
+			System.out.println(indexToDelete + ", " + member);
+
+			switch (member) {
+			case "place":
+				try {
+					log.place = new JsonParser().parse(req.getReader())
+							.getAsString();
+				} catch (Exception e) {
+					resp.sendError(400,
+							"Bad data. Expecting JSON String element.");
+				}
+				break;
+			case "conditions":
+				try {
+					log.conditions = new JsonParser().parse(req.getReader())
+							.getAsString();
+				} catch (Exception e) {
+					resp.sendError(400,
+							"Bad data. Expecting JSON String element.");
+				}
+				break;
+			case "systems":
+				System.out.println(indexToDelete);
+				if (indexToDelete >= 0) {
+					log.systems.remove(indexToDelete);
+				} else {
+					JsonElement elem = new JsonParser().parse(req.getReader());
+					if (elem.isJsonPrimitive()) {
+						log.systems.add(elem.getAsString());
+						System.out.println(elem.getAsString());
+					} else {
+						resp.sendError(400,
+								"Bad data. Expecting JSON String element.");
+						return;
+					}
+				}
+				break;
+			case "team":
+				if (indexToDelete >= 0) {
+					log.team.remove(indexToDelete);
+				} else {
+					JsonElement elem = new JsonParser().parse(req.getReader());
+					if (elem.isJsonPrimitive())
+						log.team.add(elem.getAsString());
+					else {
+						resp.sendError(400,
+								"Bad data. Expecting JSON String element.");
+						return;
+					}
+				}
+				break;
+			case "objectives":
+				if (indexToDelete >= 0) {
+					log.objectives.remove(indexToDelete);
+				} else {
+					JsonElement elem = new JsonParser().parse(req.getReader());
+					if (elem.isJsonPrimitive())
+						log.objectives.add(elem.getAsString());
+					else {
+						resp.sendError(400,
+								"Bad data. Expecting JSON String element.");
+						return;
+					}
+				}
+				break;
+			case "actions":
+				if (indexToDelete >= 0) {
+					log.actions.remove(indexToDelete);
+				} else {
+					JsonElement elem = new JsonParser().parse(req.getReader());
+
+					try {
+						ActionItem item = new ActionItem();
+						item.module = elem.getAsJsonObject().get("module")
+								.getAsString();
+						item.text = elem.getAsJsonObject().get("text")
+								.getAsString();
+						log.actions.add(item);
+					} catch (Exception e) {
+						resp.sendError(400,
+								"Bad data. Expecting JSON Object element with fields 'module' and 'text'");
+						return;
+					}
+				}
+				break;
+			case "log":
+				if (indexToDelete >= 0) {
+					log.log.remove(indexToDelete);
+				} else {
+					JsonElement elem = new JsonParser().parse(req.getReader());
+					try {
+						LogEntry entry = new LogEntry();
+						entry.author = elem.getAsJsonObject().get("author")
+								.getAsString();
+						entry.text = elem.getAsJsonObject().get("text")
+								.getAsString();
+						if (elem.getAsJsonObject().get("dataUrl") != null) {
+							entry.dataUrl = elem.getAsJsonObject()
+									.get("dataUrl").getAsString();
+						}
+						if (elem.getAsJsonObject().get("tags") != null) {
+							JsonArray arr = elem.getAsJsonObject().get("tags")
+									.getAsJsonArray();
+							for (JsonElement el : arr)
+								entry.tags.add(el.getAsString());
+						}
+						log.log.add(entry);
+					} catch (Exception e) {
+						resp.sendError(
+								400,
+								"Bad data. Expecting JSON Object element with fields 'author' 'text', 'dataUrl', tags[]");
+						return;
+					}
+				}
+				break;
+			default:
+				resp.sendError(400, "Member " + member
+						+ " does not exist in logbook.");
+				return;
+			}
+		}
+
+		Store.ofy().save().entity(log);
+
+		System.out.println(log.asJson());
+		resp.setContentType("application/json");
+		resp.getWriter().write(log.asJson());
+		resp.getWriter().close();
+
+	}
+
+	private MissionLog getLogBook(HttpServletRequest req) {
+		String[] pathParts = req.getPathInfo().split("[/\\.]+");
+		try {
+			return Store.ofy().load().type(MissionLog.class).id(pathParts[1])
+					.now();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	private String getExtension(HttpServletRequest req) {
+		String[] pathParts = req.getPathInfo().split("/");
+
+		if (pathParts.length < 2)
+			return null;
+		try {
+			return pathParts[1].toLowerCase().split("\\.")[1];
+		} catch (Exception e) {
+			return null;
+		}
+
+	}
+
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		doPost(req, resp);
+	}
 
 	/**
 	 * Parse the URL path and respond accordingly
 	 */
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		
-		if (req.getPathInfo() == null || req.getPathInfo().equals("/")) {
-			resp.setContentType("text/html");
+
+		if (req.getPathInfo() == null
+				|| req.getPathInfo().split("/").length < 2) {
 			listLogs(req, resp);
-		} else {
-			String date = new SimpleDateFormat("YYYY-MM-dd").format(new Date());
-			MissionLog log = Store.ofy().load().type(MissionLog.class).id(date).now();
-			
-			if (log == null)
-				resp.sendError(404, "No such mission log.");
-			
-			
-			
-			
-			getSystem(req.getPathInfo().substring(1), req, resp);
-		}
-		
-		
-		
-
-		if (req.getPathInfo() == null || req.getPathInfo().equals("/")) {
-			listLogs(req, resp);
-		} else {
-			getSystem(req.getPathInfo().substring(1), req, resp);
-		}
-		resp.getWriter().close();
-	}
-
-	/**
-	 * Store a system state.
-	 */
-	@Override
-	protected void doPut(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		try {
-			HubSystem s = JsonUtils.getGsonInstance().fromJson(
-					new InputStreamReader(req.getInputStream()),
-					HubSystem.class);
-			s.setUpdated_at(new Date());
-			Store.ofy().save().entity(s).now();
-			Logger.getLogger(getClass().getName()).log(Level.FINE, "System " + s.getName() + " was updated.");
-			resp.setStatus(200);
-			resp.setContentType("application/json");
-			resp.getWriter().write(JsonUtils.getGsonInstance().toJson(s));
 			resp.getWriter().close();
-		} catch (Exception e) {
-			e.printStackTrace();
-			Logger.getLogger(getClass().getName()).log(Level.WARNING, "Error handling " + req.getPathInfo(), e);
-			resp.setStatus(400);
-			resp.getWriter().close();
-		}
+			return;
+		} else {
+			String[] pathParts = req.getPathInfo().split("[/\\.]+");
 
-	}
-
-	/**
-	 * Retrieve a specific system (provided on the path)
-	 */
-	public void getSystem(String system, HttpServletRequest req,
-			HttpServletResponse resp) throws IOException {
-
-		if (system.equals("active"))
-			listActiveSystems(req, resp);
-		else {
-			try {
-				long id = Long.parseLong(system);
-				HubSystem s = Store.ofy().load().type(HubSystem.class).id(id).now();
-				if (s == null)
-					resp.setStatus(404);
-				else {
-					resp.setContentType("application/json");
-					resp.setStatus(200);
-					resp.getWriter().write(JsonUtils.getGsonInstance().toJson(s));
+			if (pathParts[1].equals("create")) {
+				MissionLog log = new MissionLog();
+				MissionLog existing = Store.ofy().load().type(MissionLog.class)
+						.id(log.date).now();
+				if (existing == null) {
+					Store.ofy().save().entity(log).now();
 				}
-			} catch (Exception e) {
-				e.printStackTrace();
-				Logger.getLogger(getClass().getName()).log(Level.WARNING, "Error handling " + req.getPathInfo(), e);
-				resp.setStatus(400);
+				resp.sendRedirect(req.getServletPath() + "/" + log.date);
+				return;
+			}
+
+			MissionLog log = getLogBook(req);
+			if (log == null) {
+				resp.sendError(404, "The requested log book does not exist.");
+				return;
+			} else {
+				String extension = getExtension(req);
+				switch (extension) {
+				case "html":
+					resp.setContentType("text/html");
+					resp.getWriter().write(log.asHtml());
+					resp.getWriter().close();
+					break;
+				case "md":
+					resp.setContentType("text/plain");
+					resp.getWriter().write(log.asMarkDown());
+					resp.getWriter().close();
+					break;
+				default:
+					resp.setContentType("application/json");
+					resp.getWriter().write(log.asJson());
+					resp.getWriter().close();
+					break;
+				}
 			}
 		}
-		resp.getWriter().close();
 	}
 
-	/**
-	 * Retrieve only systems that were updated on the last 3 days
-	 */
-	public void listActiveSystems(HttpServletRequest req,
-			HttpServletResponse resp) throws IOException {
-		Date d = new Date(System.currentTimeMillis() - 1000 * 3600 * 24);
-		List<HubSystem> systems = Store.ofy().load().type(HubSystem.class)
-				.filter("updated_at >=", d).order("-updated_at").list();
-		resp.setContentType("application/json");
-		resp.setStatus(200);
-		resp.getWriter().write(JsonUtils.getGsonInstance().toJson(systems));
-		resp.getWriter().close();
-	}
-
-	/**
-	 * Retrieve all stored systems (new and old)
-	 */
 	public void listLogs(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		List<HubSystem> systems = Store.ofy().load().type(HubSystem.class)
-				.order("-updated_at").list();
-		resp.setContentType("application/json");
-		resp.setStatus(200);
-		resp.getWriter().write(JsonUtils.getGsonInstance().toJson(systems));
-		resp.getWriter().close();
+		resp.setContentType("text/html");
+		List<MissionLog> logs = Store.ofy().load().type(MissionLog.class)
+				.list();
+		resp.getWriter().write("<html>\n");
+		resp.getWriter().write("<h1>Mission log books</h1>\n");
+		resp.getWriter().write("<ul>\n");
+		for (MissionLog l : logs) {
+			resp.getWriter().write(
+					"<li><a href=\"" + req.getServletPath() + "/" + l.date
+							+ "\">" + l.date + "</li>\n");
+		}
+		resp.getWriter().write("</ul>\n");
 	}
 }
