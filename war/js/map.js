9c97314f24c2b258299696c1c84cb8a2f3b41736
Mauro Brandão <mauro@lsts.pt>
2015-04-17 11:21:31 +0100

    Added context menu to index.html

--- war/js/map.js
+++ war/js/map.js
@@ -247,3 +247,35 @@ ripplesRef.child('assets').on(
 				markers[name].addTo(map);
 			}
 		});
+
+map.on('contextmenu',function(e){
+    //alert("plugin run!");
+	
+	$(function(){
+	    $.contextMenu({
+	        selector: '.context-menu-one', 
+	        callback: function(key, options) {
+	            var m = "global: " + key;
+	            window.console && console.log(m) || alert(m); 
+	        },
+	        items: {
+	            "edit": {
+	                name: "Edit", 
+	                icon: "edit", 
+	                // superseeds "global" callback
+	                callback: function(key, options) {
+	                    var m = "edit was clicked";
+	                    window.console && console.log(m) || alert(m); 
+	                }
+	            },
+	            "cut": {name: "Cut", icon: "cut"},
+	            "copy": {name: "Copy", icon: "copy"},
+	            "paste": {name: "Paste", icon: "paste"},
+	            "delete": {name: "Delete", icon: "delete"},
+	            "sep1": "---------",
+	            "quit": {name: "Quit", icon: "quit"}
+	        }
+	    });
+	});
+	
+});
