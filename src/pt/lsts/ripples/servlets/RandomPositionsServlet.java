d907660f2255eaf6c4554e74aadc8e0fbcd12213
José Pinto (Porto) <zepinto@gmail.com>
2015-06-15 14:56:51 +0100

    Reduced the amount of movement in random positions.

--- src/pt/lsts/ripples/servlets/RandomPositionsServlet.java
+++ src/pt/lsts/ripples/servlets/RandomPositionsServlet.java
@@ -23,8 +23,8 @@ public class RandomPositionsServlet extends HttpServlet {
 		
 		double lat = r.nextGaussian() * 0.1 + 41;
 		double lon = r.nextGaussian() * 0.1 -8;
-		double movement_x = r.nextGaussian() * 10 - 5;
-		double movement_y = r.nextGaussian() * 10 - 5;
+		double movement_x = r.nextGaussian() * 0.00001 - 0.000005;
+		double movement_y = r.nextGaussian() * 0.00001 - 0.000005;
 		resp.setContentType("text/plain");
 		for (int i = -10; i < 1; i++) {
 			SystemPosition pos = new SystemPosition();
