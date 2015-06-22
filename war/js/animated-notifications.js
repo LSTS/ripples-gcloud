2fd589d69af5867427b28e0dce39a443a284b3c3
Mauro Brandão <mauro@lsts.pt>
2015-06-22 15:52:51 +0100

    Added loadData to get the logbook data for the current day.

--- war/js/animated-notifications.js
+++ war/js/animated-notifications.js
@@ -1,3 +1,5 @@
+var log_entry = {};
+
 $(window).load(function () {
 	
 function resize(){$('#notifications').height(window.innerHeight - 50);}
@@ -9,6 +11,24 @@ $('.close').click(function(){$(this).parent().fadeOut(200);});
 }
 refresh_close();
 
+function loadData(){
+	var d = new Date();
+	var month = d.getMonth() + 1;
+	var day = d.getDate();
+	var date = d.getFullYear() + '-'
+			+ (month < 10 ? '0' : '') + month + '-'
+			+ (day < 10 ? '0' : '') + day;
+	var path = "/logbook/" +date+".json";
+	
+	var log_entry_json = $.getJSON(path, function(data) {
+		$.each(data, function(i, item) {
+			log_entry[item.log] = item;
+			console.log("log_entry["+i+"]: "+log_entry[item.log]);
+		});
+	});	
+}
+loadData();
+
 var Notification = function(){};
 Notification.prototype = {
 	  init: function(widget_name){
@@ -16,7 +36,7 @@ Notification.prototype = {
 	    this.widget_name = widget_name;
 	    console.log("name: "+widget_name);
 	    
-	    var bottom_center = '<div id='+widget_name+' class="notifications-bottom-right-tab"><div id="notifications-bottom-right-tab-close" class="close"><span>x</span></div><div id="notifications-bottom-right-tab-avatar"><img src="images/logo_hub.png" width="84" height="60" /></div><div id="notifications-bottom-right-tab-right"><div id="notifications-bottom-right-tab-right-title"><span>Ripples</span> sent you a message</div><div id="notifications-bottom-right-tab-right-text">This is a sample notification that <br> will appear the right bottom corner.</div></div></div>';
+	    var bottom_center = '<div id='+widget_name+' class="notifications-bottom-right-tab"><div id="notifications-bottom-right-tab-close" class="close"><span>x</span></div><div id="notifications-bottom-right-tab-avatar"><img src="images/logo_hub.png" width="84" height="60" /></div><div id="notifications-bottom-right-tab-right"><div id="notifications-bottom-right-tab-right-title"><span>Ripples '+widget_name+'</span> sent you a message</div><div id="notifications-bottom-right-tab-right-text">This is a sample notification that <br> will appear the right bottom corner.</div></div></div>';
 	    
 	    $("#notifications-bottom-right").html();
 	    $("#notifications-bottom-right").append(bottom_center);
@@ -33,11 +53,7 @@ Notification.prototype = {
 	  }
 	};
 
-var note1 = new Notification();
-note1.init("z1");
-var note2 = new Notification();
-note2.init("z2");
-var note3 = new Notification();
-note3.init("z3");
+var note = new Notification();
+note.init("z");
 
 });
