9af98c246be0f174e2efc87a6057c9115fc8434c
Mauro Brandão <mauro@lsts.pt>
2015-10-01 13:53:46 +0100

    Added GCMNotification.java multicast support.

--- src/pt/lsts/ripples/servlets/GCMNotification.java
+++ src/pt/lsts/ripples/servlets/GCMNotification.java
@@ -1,18 +1,12 @@
 package pt.lsts.ripples.servlets;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.*;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 
-import com.google.android.gcm.server.Message;
-import com.google.android.gcm.server.Result;
-import com.google.android.gcm.server.Sender;
+import com.google.android.gcm.server.*;
 
 
 public class GCMNotification extends HttpServlet {
@@ -20,7 +14,8 @@ public class GCMNotification extends HttpServlet {
 
 	// Put your Google API Server Key here
 	private static final String GOOGLE_SERVER_KEY = "AIzaSyBi_CwWtK2VllJ-e03HRDl0Xr0e7ylgAQE";
-	static final String MESSAGE_KEY = "message";	
+	static final String MESSAGE_KEY = "message";
+	static final String REG_ID_STORE = "GCMRegId.txt";
 
 	public GCMNotification() {
 		super();
@@ -35,36 +30,31 @@ public class GCMNotification extends HttpServlet {
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
-
-		Result result = null;
+		MulticastResult result = null;
 
 		String share = request.getParameter("shareRegId");
 
 		// GCM RedgId of Android device to send push notification
-		String regId = "";
+
 		if (share != null && !share.isEmpty()) {
-			regId = request.getParameter("regId");
-			PrintWriter writer = new PrintWriter("GCMRegId.txt");
-			writer.println(regId);
-			writer.close();
+			writeToFile(request.getParameter("regId"));
 			request.setAttribute("pushStatus", "GCM RegId Received.");
 			request.getRequestDispatcher("push.jsp")
 					.forward(request, response);
 		} else {
 
 			try {
-				BufferedReader br = new BufferedReader(new FileReader(
-						"GCMRegId.txt"));
-				regId = br.readLine();
-				br.close();
+
 				String userMessage = request.getParameter("message");
 				Sender sender = new Sender(GOOGLE_SERVER_KEY);
 				Message message = new Message.Builder().timeToLive(30)
-						.delayWhileIdle(true).addData(MESSAGE_KEY, userMessage).build();
-				System.out.println("regId: " + regId);
-				System.out.println("message: " + message);
-				result = sender.send(message, regId, 1);
-				System.out.println("result: " + result);
+						.delayWhileIdle(true).addData(MESSAGE_KEY, userMessage)
+						.build();
+				Set<String> regIdSet = readFromFile();
+				System.out.println("regId: " + regIdSet);
+				List<String> regIdList = new ArrayList<String>();
+				regIdList.addAll(regIdSet);
+				result = sender.send(message, regIdList, 1);
 				request.setAttribute("pushStatus", result.toString());
 			} catch (IOException ioe) {
 				ioe.printStackTrace();
@@ -77,5 +67,28 @@ public class GCMNotification extends HttpServlet {
 			request.getRequestDispatcher("push.jsp")
 					.forward(request, response);
 		}
+	}
+
+	private void writeToFile(String regId) throws IOException {
+		Set<String> regIdSet = readFromFile();
+
+		if (!regIdSet.contains(regId)) {
+			PrintWriter out = new PrintWriter(new BufferedWriter(
+					new FileWriter(REG_ID_STORE, true)));
+			out.println(regId);
+			out.close();
+		}
+
+	}
+
+	private Set<String> readFromFile() throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(REG_ID_STORE));
+		String regId = "";
+		Set<String> regIdSet = new HashSet<String>();
+		while ((regId = br.readLine()) != null) {
+			regIdSet.add(regId);
+		}
+		br.close();
+		return regIdSet;
 	}
 }
