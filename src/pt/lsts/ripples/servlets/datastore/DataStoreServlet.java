2b4e07021d803b2039b51817b2349bf163173449
Jose Pinto <zepinto@gmail.com>
2016-06-12 21:36:08 +0100

    Fixed retrieval of commands.

--- src/pt/lsts/ripples/servlets/datastore/DataStoreServlet.java
+++ src/pt/lsts/ripples/servlets/datastore/DataStoreServlet.java
@@ -51,53 +51,68 @@ public class DataStoreServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-		resp.setContentType("application/lsf");	
-		
+		resp.setContentType("application/lsf");
+
 		ArrayList<DataSample> samples = new ArrayList<>();
+		IMCMessage retrievedCommands = null;
 
 		try {
 			resp.setStatus(200);
 			IMCInputStream in = new IMCInputStream(req.getInputStream(), IMCDefinition.getInstance());
 			IMCMessage msg = in.readMessage();
 			in.close();
-			
+
 			samples.addAll(process(msg));
-			
+
 			ArrayList<Command> cmds = extractCommands(msg);
-			
+
 			ArrayList<HistoricDatum> data = new ArrayList<>();
 			for (DataSample sample : samples)
 				data.add(convert(sample));
 			Store.ofy().save().entities(data).now();
 			Store.ofy().save().entities(cmds).now();
-			
-			System.out.println("Added " + samples.size() + " samples and "+cmds.size()+" commands from "+msg.getSourceName()+" to cloud store.");
-			
-			IMCMessage m = dataFor(msg.getSrc());
+
+			System.out.println("Added " + samples.size() + " samples and " + cmds.size() + " commands from "
+					+ msg.getSourceName() + " to cloud store.");
+
+			retrievedCommands = dataFor(msg.getSrc(), true);
 			IMCOutputStream ios = new IMCOutputStream(resp.getOutputStream());
-			ios.writeMessage(m);
+			ios.writeMessage(retrievedCommands);
 			resp.getOutputStream().close();
-			
+
 		} catch (Exception e) {
-			resp.setStatus(500);	
+			resp.setStatus(500);
 			PrintWriter out = resp.getWriter();
 			e.printStackTrace(out);
-			out.close();				
+			out.close();
+			if (retrievedCommands != null) {
+				try {
+					ArrayList<Command> cmds = extractCommands(retrievedCommands);
+					Store.ofy().save().entities(cmds).now();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+
 		}
 	}
-	
-	private IMCMessage dataFor(int dst) {
+
+	private IMCMessage dataFor(int dst, boolean remove) {
 		HistoricData data = new HistoricData();
 		data.setDst(dst);
 		Vector<RemoteData> cmds = data.getData();
-		for (Command cmd : Store.ofy().load().type(Command.class).filter("imc_id_dest==", dst).iterable()) {
+		for (Command cmd : Store.ofy().load().type(Command.class).filter("imc_id_dest", dst).iterable()) {
+			
 			try {
-				cmds.add(convert(cmd));
-			}
-			catch (Exception e) {
+				if (cmd.timeout * 1000 > System.currentTimeMillis())
+					cmds.add(convert(cmd));
+				if (remove)
+					Store.ofy().delete().entity(cmd);
+			} catch (Exception e) {
 				e.printStackTrace();
-			}			
+			}
 		}
+
 		data.setData(cmds);
 		return data;
 	}
@@ -107,46 +122,43 @@ public class DataStoreServlet extends HttpServlet {
 		sample.setLatDegs(datum.lat);
 		sample.setLonDegs(datum.lon);
 		sample.setzMeters(datum.z);
-		sample.setSource((int)datum.imc_id);
+		sample.setSource((int) datum.imc_id);
 		sample.setTimestampMillis(datum.timestamp.getTime());
 		if (datum instanceof CTDSample) {
-			CTDSample ctdDatum = (CTDSample)datum;
+			CTDSample ctdDatum = (CTDSample) datum;
 			HistoricCTD ctd = new HistoricCTD();
 			ctd.setConductivity(ctdDatum.conductivity);
 			ctd.setDepth(ctdDatum.depth);
 			ctd.setTemperature(ctdDatum.temperature);
 			sample.setSample(ctd);
-		}
-		else if (datum instanceof TelemetrySample) {
-			TelemetrySample telDatum = (TelemetrySample)datum;
+		} else if (datum instanceof TelemetrySample) {
+			TelemetrySample telDatum = (TelemetrySample) datum;
 			HistoricTelemetry tel = new HistoricTelemetry();
-			tel.setRoll((int)(telDatum.roll * 65535 / 360));
-			tel.setPitch((int)(telDatum.pitch * 65535 / 360));
-			tel.setYaw((int)(telDatum.yaw * 65535 / 360));
+			tel.setRoll((int) (telDatum.roll * 65535 / 360));
+			tel.setPitch((int) (telDatum.pitch * 65535 / 360));
+			tel.setYaw((int) (telDatum.yaw * 65535 / 360));
 			tel.setSpeed((short) (telDatum.speed * 10));
 			sample.setSample(tel);
-		}
-		else if (datum instanceof EventSample) {
+		} else if (datum instanceof EventSample) {
 			EventSample evtDatum = (EventSample) datum;
 			HistoricEvent evt = new HistoricEvent();
 			evt.setText(evtDatum.text);
-			evt.setType(evtDatum.error? TYPE.ERROR : TYPE.INFO);
+			evt.setType(evtDatum.error ? TYPE.ERROR : TYPE.INFO);
 			sample.setSample(evt);
+		} else {
+			System.err.println("Unrecongnized type of datum: " + datum.getClass().getSimpleName());
 		}
-		else {
-			System.err.println("Unrecongnized type of datum: "+datum.getClass().getSimpleName());
-		}
-		
+
 		return sample;
 	}
-	
+
 	private HistoricDatum convert(DataSample sample) {
 		HistoricDatum datum;
 		int type = -1;
 		if (sample.getSample() != null)
 			type = sample.getSample().getMgid();
 
-		switch(sample.getSample().getMgid()) {
+		switch (sample.getSample().getMgid()) {
 		case HistoricCTD.ID_STATIC: {
 			HistoricCTD original = new HistoricCTD(sample.getSample());
 			CTDSample converted = new CTDSample();
@@ -190,68 +202,67 @@ public class DataStoreServlet extends HttpServlet {
 	}
 
 	private ArrayList<DataSample> process(IMCMessage msg) throws Exception {
-		switch(msg.getMgid()) {
-		case HistoricData.ID_STATIC: 
+		switch (msg.getMgid()) {
+		case HistoricData.ID_STATIC:
 			return DataSample.parseSamples(new HistoricData(msg));
 		default:
-			throw new Exception("Message type is not supported: "+msg.getAbbrev());
+			throw new Exception("Message type is not supported: " + msg.getAbbrev());
 		}
 	}
-	
+
 	private Command convert(RemoteCommand sample) {
 		Command ret = new Command();
-		ret.cmd = new Blob(sample.toByteArray());
-		ret.imc_id_dest = sample.getDst();
-		ret.imc_id_source = sample.getSrc();
+		ret.cmd = new Blob(sample.getCmd().toByteArray());
+		ret.imc_id_dest = sample.getDestination();
+		ret.imc_id_source = sample.getOriginalSource();
 		ret.timeout = sample.getTimeout();
 		ret.timestamp = sample.getDate();
 		return ret;
 	}
-	
+
 	private RemoteCommand convert(Command cmd) throws IOException {
 		RemoteCommand ret = new RemoteCommand();
-		IMCInputStream iis = new IMCInputStream(new ByteArrayInputStream(cmd.cmd.getBytes()), IMCDefinition.getInstance());
+		IMCInputStream iis = new IMCInputStream(new ByteArrayInputStream(cmd.cmd.getBytes()),
+				IMCDefinition.getInstance());
 		ret.setCmd(iis.readMessage());
 		iis.close();
-		ret.setDestination((int)cmd.imc_id_dest);
+		ret.setDestination((int) cmd.imc_id_dest);
 		ret.setTimeout(cmd.timeout);
-		ret.setTimestamp(cmd.timestamp.getTime()/1000.0);
+		ret.setTimestamp(cmd.timestamp.getTime() / 1000.0);
 		return ret;
 	}
 
 	private ArrayList<Command> extractCommands(IMCMessage msg) throws Exception {
 		ArrayList<Command> ret = new ArrayList<>();
-		switch(msg.getMgid()) {
+		switch (msg.getMgid()) {
 		case HistoricData.ID_STATIC:
 			HistoricData hist = new HistoricData(msg);
 			for (RemoteCommand rcmd : DataSample.parseCommands(hist)) {
 				if (rcmd.getTimeout() * 1000 > System.currentTimeMillis())
 					ret.add(convert(rcmd));
 				else
-					System.out.println("Discarding expired command: "+ret);
+					System.out.println("Discarding expired command: " + ret);
 			}
 			return ret;
 		default:
-			throw new Exception("Message type is not supported: "+msg.getAbbrev());
+			throw new Exception("Message type is not supported: " + msg.getAbbrev());
 		}
 	}
-	
-	
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		String type = "lsf";
-		
+
 		if (req.getPathInfo() == null || req.getPathInfo().equals("/")) {
 			listHistoricData(req, resp, MAX_RESULTS, type);
 			resp.getWriter().close();
 			return;
 		}
-		
+
 		if (!req.getParameterNames().hasMoreElements()) {
 			if (req.getPathInfo().equalsIgnoreCase("/json"))
-				listHistoricData(req, resp, MAX_RESULTS, "json");		
+				listHistoricData(req, resp, MAX_RESULTS, "json");
 			else if (req.getPathInfo().equalsIgnoreCase("/lsf"))
 				listHistoricData(req, resp, MAX_RESULTS, "lsf");
 			else if (req.getPathInfo().equalsIgnoreCase("/xml"))
@@ -265,16 +276,16 @@ public class DataStoreServlet extends HttpServlet {
 			}
 			return;
 		}
-					
-		if (!parameterMatch(req,  Arrays.asList("type", "source", "since"))) {
+
+		if (!parameterMatch(req, Arrays.asList("type", "source", "since"))) {
 			PrintWriter out = resp.getWriter();
 			printInvalid(resp, out);
 			resp.getWriter().close();
 			return;
 		}
-		
+
 		if (req.getPathInfo().equalsIgnoreCase("/json"))
-			filterHistoricData(req, resp, MAX_RESULTS, "json");		
+			filterHistoricData(req, resp, MAX_RESULTS, "json");
 		else if (req.getPathInfo().equalsIgnoreCase("/imc"))
 			filterHistoricData(req, resp, MAX_RESULTS, "imc");
 		else if (req.getPathInfo().equalsIgnoreCase("/xml"))
@@ -288,9 +299,9 @@ public class DataStoreServlet extends HttpServlet {
 	private void printInvalid(HttpServletResponse resp, PrintWriter out) {
 		resp.setContentType("text/plain");
 		out.println("Invalid request. Examples:");
-		out.println("/json?type=100&source=30&since="+(System.currentTimeMillis()-3600*1000));
+		out.println("/json?type=100&source=30&since=" + (System.currentTimeMillis() - 3600 * 1000));
 		out.println("/xml");
-		out.println("/lsf&since="+(System.currentTimeMillis()-3600*1000));
+		out.println("/lsf&since=" + (System.currentTimeMillis() - 3600 * 1000));
 		resp.setStatus(400);
 		out.close();
 	}
@@ -310,7 +321,8 @@ public class DataStoreServlet extends HttpServlet {
 		return true;
 	}
 
-	private void filterHistoricData(HttpServletRequest req, HttpServletResponse resp, int limit, String contenttype) throws ServletException, IOException {
+	private void filterHistoricData(HttpServletRequest req, HttpServletResponse resp, int limit, String contenttype)
+			throws ServletException, IOException {
 		resp.setContentType("text/plain");
 		resp.setStatus(200);
 		ArrayList<Filter> list = new ArrayList<>();
@@ -319,9 +331,7 @@ public class DataStoreServlet extends HttpServlet {
 		if (type != null) {
 			try {
 				long typeValue = Long.parseLong(type);
-				Filter typeFilter = new FilterPredicate("sample_type",
-						FilterOperator.EQUAL,
-						typeValue);
+				Filter typeFilter = new FilterPredicate("sample_type", FilterOperator.EQUAL, typeValue);
 				list.add(typeFilter);
 			} catch (NumberFormatException e) {
 				printInvalid(resp, resp.getWriter());
@@ -334,9 +344,7 @@ public class DataStoreServlet extends HttpServlet {
 			try {
 				long systemValue = Long.parseLong(system);
 
-				Filter systemFilter = new FilterPredicate("imc_id",
-						FilterOperator.EQUAL,
-						systemValue);
+				Filter systemFilter = new FilterPredicate("imc_id", FilterOperator.EQUAL, systemValue);
 				list.add(systemFilter);
 			} catch (NumberFormatException e) {
 				printInvalid(resp, resp.getWriter());
@@ -345,14 +353,12 @@ public class DataStoreServlet extends HttpServlet {
 		}
 		String since = req.getParameter("since");
 		if (since == null)
-			since = ""+(System.currentTimeMillis()-3600*1000)/1000;
+			since = "" + (System.currentTimeMillis() - 3600 * 1000) / 1000;
 
 		try {
 			long sinceValue = Long.parseLong(since);
 			Date date = new Date(sinceValue);
-			Filter sinceFilter = new FilterPredicate("timestamp",
-					FilterOperator.GREATER_THAN,
-					date);
+			Filter sinceFilter = new FilterPredicate("timestamp", FilterOperator.GREATER_THAN, date);
 			list.add(sinceFilter);
 		} catch (NumberFormatException e) {
 			e.printStackTrace();
@@ -363,72 +369,64 @@ public class DataStoreServlet extends HttpServlet {
 		Filter filter = null;
 		if (list.size() == 1)
 			filter = list.get(0);
-		else 
+		else
 			filter = CompositeFilterOperator.and(list);
 
-		List<HistoricDatum> historicData = Store.ofy().load().type(HistoricDatum.class)
-				.filter(filter)
-				.order("timestamp")
-				.limit(MAX_RESULTS)
-				.list();			
+		List<HistoricDatum> historicData = Store.ofy().load().type(HistoricDatum.class).filter(filter)
+				.order("timestamp").limit(MAX_RESULTS).list();
 
 		DataStore store = new DataStore();
 		HistoricData data;
-		
+
 		if (historicData.isEmpty()) {
 			HistoricDatum lastSample = Store.ofy().load().type(HistoricDatum.class).order("-timestamp").first().now();
 			data = new HistoricData();
 			data.setBaseTime(System.currentTimeMillis() / 1000);
 			if (lastSample != null)
 				data.setBaseTime(lastSample.timestamp.getTime() / 1000);
-		}
-		else {
+		} else {
 			for (HistoricDatum datum : historicData) {
 				if (datum.getClass() != HistoricDatum.class)
 					store.addSample(convert(datum));
-			}				
+			}
 			try {
 				data = store.pollData(0, 65000);
-			}
-			catch (Exception e) {
+			} catch (Exception e) {
 				throw new ServletException(e);
 			}
 		}
-		
+
 		serveHistoricData(data, contenttype, resp);
 	}
 
-	private void listHistoricData(HttpServletRequest req, HttpServletResponse resp, int limit, String type) throws ServletException, IOException {
+	private void listHistoricData(HttpServletRequest req, HttpServletResponse resp, int limit, String type)
+			throws ServletException, IOException {
 
-		List<HistoricDatum> historicData = Store.ofy().load().type(HistoricDatum.class)
-				.order("-timestamp")
-				.limit(limit)
+		List<HistoricDatum> historicData = Store.ofy().load().type(HistoricDatum.class).order("-timestamp").limit(limit)
 				.list();
 
 		resp.setStatus(200);
 
 		DataStore store = new DataStore();
 		HistoricData data;
-		
+
 		if (historicData.isEmpty()) {
 			data = new HistoricData();
-			data.setBaseTime(System.currentTimeMillis()/1000);
-		}
-		else {
+			data.setBaseTime(System.currentTimeMillis() / 1000);
+		} else {
 			for (HistoricDatum datum : historicData)
 				if (datum.getClass() != HistoricDatum.class)
 					store.addSample(convert(datum));
 			try {
 				data = store.pollData(0, 65000);
-			}
-			catch (Exception e) {
+			} catch (Exception e) {
 				throw new ServletException(e);
 			}
 		}
-		
-		serveHistoricData(data, type, resp);	
+
+		serveHistoricData(data, type, resp);
 	}
-	
+
 	private void serveHistoricData(HistoricData data, String contentType, HttpServletResponse resp) throws IOException {
 		switch (contentType) {
 		case "json":
@@ -443,13 +441,14 @@ public class DataStoreServlet extends HttpServlet {
 			break;
 		case "html":
 			resp.setContentType("text/html");
-			resp.getWriter().println("<h3>Number of samples: "+data.getData().size()+"</h3>");
-			resp.getWriter().println("<h3>Message size: "+data.getPayloadSize()+"</h3>");
-			resp.getWriter().println("<h3>Last time: "+new Date((long)(data.getBaseTime()*1000))+"</h3>");
+			resp.getWriter().println("<h3>Number of samples: " + data.getData().size() + "</h3>");
+			resp.getWriter().println("<h3>Message size: " + data.getPayloadSize() + "</h3>");
+			resp.getWriter().println("<h3>Last time: " + new Date((long) (data.getBaseTime() * 1000)) + "</h3>");
 			int t = 0;
 			if (!data.getData().isEmpty())
-				t = ((HistoricSample)data.getData().lastElement()).getT();
-			resp.getWriter().println("<h3>Initial time: "+new Date((long)((data.getBaseTime()+t)*1000))+"</h3>");
+				t = ((HistoricSample) data.getData().lastElement()).getT();
+			resp.getWriter()
+					.println("<h3>Initial time: " + new Date((long) ((data.getBaseTime() + t) * 1000)) + "</h3>");
 			resp.getWriter().println(IMCUtil.getAsHtml(data));
 			resp.getWriter().close();
 			break;
@@ -458,7 +457,7 @@ public class DataStoreServlet extends HttpServlet {
 			IMCOutputStream ios = new IMCOutputStream(resp.getOutputStream());
 			ios.writeMessage(data);
 			resp.getOutputStream().close();
-			break;		
+			break;
 		}
 	}
 }
