6e0316026fa62b210d0f60103e55d93732bd122d
José Pinto <zepinto@gmail.com>
2018-04-29 11:54:57 +0100

    IridiumServlet: Properly ignore messages sent using irsim uri.

--- src/pt/lsts/ripples/servlets/IridiumServlet.java
+++ src/pt/lsts/ripples/servlets/IridiumServlet.java
@@ -42,8 +42,13 @@ public class IridiumServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 
+		boolean simulation = req.getRequestURI().contains("irsim");
+		if (simulation) {
+			Logger.getLogger(getClass().getName()).warning("This request is simulated.");
+		}
+			
 		if (req.getContentType().equals("application/hub")) {
-			sendInlineMessage(req, resp);
+			sendInlineMessage(req, resp, !simulation);
 		} else {
 			Logger.getLogger(getClass().getName()).info(
 					"Ignoring request with wrong content type.");
@@ -54,7 +59,7 @@ public class IridiumServlet extends HttpServlet {
 	}
 
 	private void sendInlineMessage(HttpServletRequest req,
-			HttpServletResponse resp) throws IOException {
+			HttpServletResponse resp, boolean send) throws IOException {
 
 		StringBuilder sb = new StringBuilder();
 		BufferedReader r = new BufferedReader(new InputStreamReader(
@@ -66,7 +71,7 @@ public class IridiumServlet extends HttpServlet {
 			line = r.readLine();
 		}
 
-		
+
 		try {
 			IridiumMessage m = IridiumMessage
 					.deserialize(new HexBinaryAdapter().unmarshal(sb.toString()));
@@ -80,8 +85,6 @@ public class IridiumServlet extends HttpServlet {
 			msg.setUpdated_at(new Date());
 			Store.ofy().save().entity(msg);
 
-			
-			
 			// This message is not to be sent but just posted
 			if (dst == 0 || dst == 65535) {
 				IridiumMsgHandler.setMessage(null, m);
@@ -89,49 +92,59 @@ public class IridiumServlet extends HttpServlet {
 				resp.getWriter().close();
 				return;
 			}
-			
+
 			//FIXME remove
 			IridiumMsgHandler.setMessage(null, m);
 
-			String imei = findImei(dst);
+			if (send) {
+				String imei = findImei(dst);
 
-			if (imei == null) {
-				Logger.getLogger(getClass().getName())
-				.log(Level.WARNING,
-						"Could not find IMEI address for dst. Iridium message will not be delivered.");
-				resp.getWriter().write(
-						"Could not find IMEI address for destination.");
-				resp.setStatus(500);
-				return;
+				if (imei == null) {
+					Logger.getLogger(getClass().getName())
+					.log(Level.WARNING,
+							"Could not find IMEI address for dst. Iridium message will not be delivered.");
+					resp.getWriter().write(
+							"Could not find IMEI address for destination.");
+					resp.setStatus(500);
+					return;
+				}
+
+				Pair<Integer, String> rock7Resp = IridiumUtils.sendviaRockBlock(imei, m.serialize());
+
+				if (rock7Resp.getSecond().startsWith("FAILED")) {
+					Logger.getLogger(getClass().getName()).log(Level.WARNING,
+							"Error sending Iridium message from " + src + " to " + dst+": "+rock7Resp.getSecond());
+					resp.getWriter().write("Sending to " + imei + " failed: ");
+					resp.setStatus(502);
+				} else {
+					resp.setStatus(rock7Resp.getFirst());
+					Logger.getLogger(getClass().getName()).log(Level.INFO,
+							"Sent Iridium message from " + src + " to " + dst);
+
+					IridiumMsgHandler.setMessage(null, m);
+				}
+				resp.getWriter().write(rock7Resp.getSecond());
+				resp.getWriter().close();
 			}
-
-			Pair<Integer, String> rock7Resp = IridiumUtils.sendviaRockBlock(imei, m.serialize());
-
-			if (rock7Resp.getSecond().startsWith("FAILED")) {
-				Logger.getLogger(getClass().getName()).log(Level.WARNING,
-						"Error sending Iridium message from " + src + " to " + dst+": "+rock7Resp.getSecond());
-				resp.getWriter().write("Sending to " + imei + " failed: ");
-				resp.setStatus(502);
-			} else {
-				resp.setStatus(rock7Resp.getFirst());
+			else {
 				Logger.getLogger(getClass().getName()).log(Level.INFO,
-						"Sent Iridium message from " + src + " to " + dst);
-				
-				IridiumMsgHandler.setMessage(null, m);
+						"Message has not been sent to destination (as requested)");
+				resp.setStatus(200);
+				resp.getWriter().write("Message has not been sent to destination (as requested)");				
+				resp.getWriter().close();
 			}
-			resp.getWriter().write(rock7Resp.getSecond());
-			resp.getWriter().close();
-
-		} catch (Exception e) {
+		} 
+		catch (Exception e) {
 			Logger.getLogger(getClass().getName()).log(Level.WARNING,
 					"Error sending Iridium message", e);
 			resp.setStatus(400);
 			e.printStackTrace(resp.getWriter());
 			resp.getWriter().close();
-			
-			
+
+
 			return;
 		}
+
 	}
 
 	private String findImei(int imcid) {
@@ -147,18 +160,18 @@ public class IridiumServlet extends HttpServlet {
 		if ((""+req.getPathInfo()).toUpperCase().contains("UNSUBSCRIBE")) {
 			resp.setContentType("text/html");
 			resp.setStatus(200);
-			
+
 			List<IridiumSubscription> subscribers = Store.ofy().load().type(IridiumSubscription.class).list();
 			for (IridiumSubscription s : subscribers) {
 				Store.ofy().delete().entity(s).now();
 				Logger.getGlobal().log(Level.INFO, "Removed subscription from "+s.subscriberId);
 			}
-			
+
 			resp.getWriter().write("UNSUBSCRIBED.");
 			resp.getWriter().close();
 			return;
 		}
-		
+
 		String since = req.getParameter("since");
 		String id = req.getParameter("id");
 		String data = req.getParameter("data");
@@ -250,14 +263,14 @@ public class IridiumServlet extends HttpServlet {
 	}
 
 	IMCFragmentHandler handler = new IMCFragmentHandler(IMCDefinition.getInstance());
-	
+
 	private String table() throws Exception {
 		StringBuilder sb = new StringBuilder();
 		Date start = new Date(System.currentTimeMillis() - 1000 * 3600 * 72);
 		List<HubIridiumMsg> msgs = Store.ofy().load().type(HubIridiumMsg.class)
 				.filter("updated_at >= ", start)
 				.order("updated_at").list();
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
 		sb.append("<html>\n<body>\n<table border=1><tr><th>Date</th><th>Type</th><th>Source</th><th>Destination</th><th>Data</th></tr>\n");
@@ -268,14 +281,14 @@ public class IridiumServlet extends HttpServlet {
 
 			IridiumMessage msg = IridiumMessage
 					.deserialize(new HexBinaryAdapter().unmarshal(m.getMsg()));
-			
+
 			String type = msg.getClass().getSimpleName();
-			
+
 			if (msg instanceof ImcIridiumMessage) {
 				IMCMessage imcm = ((ImcIridiumMessage)msg).getMsg();
 				if (imcm instanceof MessagePart) {
 					MessagePart part = (MessagePart)imcm;
-					
+
 					IMCMessage result = handler.setFragment((MessagePart) imcm);
 					if (result != null)
 						type += " (part " + (1 + part.getFragNumber()) + "/"
@@ -292,7 +305,7 @@ public class IridiumServlet extends HttpServlet {
 			int src = msg.getSource();
 
 			String date = sdf.format(m.getCreated_at());
-			
+
 			String source = IMCDefinition.getInstance().getResolver().resolve(src);
 			String dest = IMCDefinition.getInstance().getResolver().resolve(dst);
 			String data = "<a href='"+m.getId()+"'>Data</a>";
@@ -303,7 +316,7 @@ public class IridiumServlet extends HttpServlet {
 		List<IridiumSubscription> subscribers = Store.ofy().load().type(IridiumSubscription.class).list();
 		for (IridiumSubscription s : subscribers)
 			sb.append("<h2>Iridium Subscriber: "+s.imei+"</h2>\n");
-		
+
 		sb.append("</body></html>\n");
 		return sb.toString();
 	}
@@ -311,7 +324,7 @@ public class IridiumServlet extends HttpServlet {
 	public static void main(String[] args) throws Exception {
 		String dstr = "1f001f00da076d037d499155ab0002fd0054fe8f020401d578595f5264d541000000ffffff020f004e5f3134333535383438393333393891020700647269667465720600537572766579060090020a0063656e7465725f6c6174031200302e373138383033393034393832393937341200302e3731383830333930343938323939373490020a0063656e7465725f6c6f6e0314002d302e313531393339393035323133363838333614002d302e31353139333939303532313336383833369002040073697a6503040038302e30040038302e3090020a006c616772616e6769616e010400747275650400747275659002040070617468050600737175617265060073717561726590020700686561";
 		IridiumMessage msg = IridiumMessage.deserialize(new HexBinaryAdapter()
-		.unmarshal(dstr));
+				.unmarshal(dstr));
 		System.out.println(msg.getSource());
 	}
 }
