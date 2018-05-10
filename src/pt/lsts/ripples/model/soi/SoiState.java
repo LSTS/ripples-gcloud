f5c4225fae6dc34c29a2feeb18cbd64d2c6a1db5
José Pinto <zepinto@gmail.com>
2018-05-10 11:47:24 +0100

    SoiState: Fixed processing of StateReports.

--- src/pt/lsts/ripples/model/soi/SoiState.java
+++ src/pt/lsts/ripples/model/soi/SoiState.java
@@ -27,27 +27,38 @@ public class SoiState {
 	@Id
 	public String name;
 
+	// Last update to this state
 	public Date lastUpdated = null;
 
+	// The json representation of the state
 	public String asset = null;
 
 	public static void updateState(StateReport msg) {
 		HubSystem vehicle = Store.ofy().load().type(HubSystem.class).id(msg.getSrc()).now();
 
 		SoiState existing = Store.ofy().load().type(SoiState.class).id(vehicle.getName()).now();
+		Asset state = new Asset(vehicle.getName());
 		if (existing == null) {
 			existing = new SoiState();
-			existing.name = vehicle.getName();				
+			existing.name = vehicle.getName();											
 		}
-		Asset state = new Asset(vehicle.getName());
+		else {
+			try {
+				state = Asset.parse(existing.asset);	
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
 		state.setState(AssetState.builder()
 				.withLatitude(msg.getLatitude())
 				.withLongitude(msg.getLongitude())
 				.withTimestamp(msg.getDate())
 				.withHeading(msg.getHeading() / (65535 * 360.0))
-				.withFuel(msg.getFuel()/255.0)
+				.withFuel(msg.getFuel()/255.0)				
 				.build());
-
+		
 		existing.asset = state.toString();
 		existing.lastUpdated = msg.getDate();
 		Store.ofy().save().entities(existing).now();
